2e52713067985f94a3080ce21f2fdeb76f6b42e9
mhw32 <me@mikewuis.me>
2017-08-19 17:09:45 -0700

    Added a predict endpoint to read form request body

--- app/views.py
+++ app/views.py
@@ -1,7 +1,13 @@
 from app import app
+from flask import request
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "ResXceptionNet API"
 
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.data
